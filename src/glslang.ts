cf1d0471bfb027ef56f823b747ee0db32e977fa4
Austin Eng <enga@chromium.org>
2020-03-23 12:48:01 -0700

    Roll glslang compiler to 0.0.15
    Fixes #33

--- src/glslang.ts
+++ src/glslang.ts
@@ -2,7 +2,7 @@ let glslang = undefined;
 export default async function() {
   if (glslang !== undefined) return glslang;
   // @ts-ignore
-  const glslangModule = await import(/* webpackIgnore: true */ 'https://unpkg.com/@webgpu/glslang@0.0.7/web/glslang.js');
+  const glslangModule = await import(/* webpackIgnore: true */ 'https://unpkg.com/@webgpu/glslang@0.0.15/dist/web-devel/glslang.js');
   glslang = await glslangModule.default();
   return glslang;
 }
