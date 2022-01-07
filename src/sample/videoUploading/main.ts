d0a6bd9b63a4fab8da45823cb351ce3b0418e4ba
Austin Eng <enga@chromium.org>
2022-01-06 18:08:06 -0800

    Update VideoUploading sample to newer sample style

---
+++ src/sample/videoUploading/main.ts
@@ -0,0 +1,129 @@
+import { makeSample, SampleInit } from '../../components/SampleLayout';
+
+import fullscreenTexturedQuadWGSL from '../../shaders/fullscreenTexturedQuad.wgsl';
+import sampleExternalTextureWGSL from '../../shaders/sampleExternalTexture.frag.wgsl';
+
+const init: SampleInit = async ({ canvasRef }) => {
+  // Set video element
+  const video = document.createElement('video');
+  video.loop = true;
+  video.autoplay = true;
+  video.muted = true;
+  video.src = require('../../../assets/video/pano.webm');
+  await video.play();
+
+  const adapter = await navigator.gpu.requestAdapter();
+  const device = await adapter.requestDevice();
+
+  if (canvasRef.current === null) return;
+
+  const context = canvasRef.current.getContext('webgpu');
+
+  const devicePixelRatio = window.devicePixelRatio || 1;
+  const presentationSize = [
+    canvasRef.current.clientWidth * devicePixelRatio,
+    canvasRef.current.clientHeight * devicePixelRatio,
+  ];
+  const presentationFormat = context.getPreferredFormat(adapter);
+
+  context.configure({
+    device,
+    format: presentationFormat,
+    size: presentationSize,
+  });
+
+  const pipeline = device.createRenderPipeline({
+    vertex: {
+      module: device.createShaderModule({
+        code: fullscreenTexturedQuadWGSL,
+      }),
+      entryPoint: 'vert_main',
+    },
+    fragment: {
+      module: device.createShaderModule({
+        code: sampleExternalTextureWGSL,
+      }),
+      entryPoint: 'main',
+      targets: [
+        {
+          format: presentationFormat,
+        },
+      ],
+    },
+    primitive: {
+      topology: 'triangle-list',
+    },
+  });
+
+  const sampler = device.createSampler({
+    magFilter: 'linear',
+    minFilter: 'linear',
+  });
+
+  function frame() {
+    const uniformBindGroup = device.createBindGroup({
+      layout: pipeline.getBindGroupLayout(0),
+      entries: [
+        {
+          binding: 1,
+          resource: sampler,
+        },
+        {
+          binding: 2,
+          resource: device.importExternalTexture({
+            source: video,
+          }),
+        },
+      ],
+    });
+
+    const commandEncoder = device.createCommandEncoder();
+    const textureView = context.getCurrentTexture().createView();
+
+    const renderPassDescriptor: GPURenderPassDescriptor = {
+      colorAttachments: [
+        {
+          view: textureView,
+          loadValue: { r: 0.0, g: 0.0, b: 0.0, a: 1.0 },
+          storeOp: 'store',
+        },
+      ],
+    };
+
+    const passEncoder = commandEncoder.beginRenderPass(renderPassDescriptor);
+    passEncoder.setPipeline(pipeline);
+    passEncoder.setBindGroup(0, uniformBindGroup);
+    passEncoder.draw(6, 1, 0, 0);
+    passEncoder.endPass();
+    device.queue.submit([commandEncoder.finish()]);
+
+    requestAnimationFrame(frame);
+  }
+  requestAnimationFrame(frame);
+};
+
+const VideoUploading: () => JSX.Element = () =>
+  makeSample({
+    name: 'Video Uploading',
+    description: 'This example shows how to upload video frame to WebGPU.',
+    init,
+    sources: [
+      {
+        name: __filename.substr(__dirname.length + 1),
+        contents: __SOURCE__,
+      },
+      {
+        name: '../../shaders/fullscreenTexturedQuad.wgsl',
+        contents: fullscreenTexturedQuadWGSL,
+        editable: true,
+      },
+      {
+        name: '../../shaders/sampleExternalTexture.wgsl',
+        contents: sampleExternalTextureWGSL,
+        editable: true,
+      },
+    ],
+    filename: __filename,
+  });
+
+export default VideoUploading;
---
+++ src/shaders/sampleExternalTexture.frag.wgsl
@@ -0,0 +1,7 @@
+[[group(0), binding(1)]] var mySampler: sampler;
+[[group(0), binding(2)]] var myTexture: texture_external;
+
+[[stage(fragment)]]
+fn main([[location(0)]] fragUV : vec2<f32>) -> [[location(0)]] vec4<f32> {
+  return textureSampleLevel(myTexture, mySampler, fragUV);
+}
