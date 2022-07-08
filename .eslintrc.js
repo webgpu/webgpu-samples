2dd03579f4f3345493a319e6ee0eac29e57c40ef
onsummer <onsummer@foxmail.com>
2022-07-09 01:10:33 +0800

    Solve conflict status

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,34 +1,3 @@
-<<<<<<< HEAD
-module.exports = {
-  parser: "@typescript-eslint/parser",
-  extends: [
-    "plugin:@typescript-eslint/recommended",
-    "plugin:react/recommended",
-    "plugin:prettier/recommended",
-    "prettier/react",
-    "prettier/@typescript-eslint"
-  ],
-  plugins: [
-    "@typescript-eslint",
-    "react",
-    "prettier"
-  ],
-  rules: {
-    "react/react-in-jsx-scope": "off",
-    "react/prop-types": "off",
-    "@typescript-eslint/no-unused-vars": ["error", { "vars": "all", "args": "after-used", "ignoreRestSiblings": true }],
-    "prettier/prettier": ["error", { "endOfLine": "auto" }]
-  },
-  globals: {
-    React: "writable"
-  },
-  settings: {
-    react: {
-      version: "detect"
-    }
-  }
-};
-=======
 module.exports = {
   parser: "@typescript-eslint/parser",
   extends: [
@@ -57,4 +26,3 @@ module.exports = {
     }
   }
 };
->>>>>>> main
