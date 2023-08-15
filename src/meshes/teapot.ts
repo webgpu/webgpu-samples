0cde8a22220e1be41e1335a5678f3ce298c11680
Kai Ninomiya <kainino@chromium.org>
2023-08-15 09:50:02 -0700

    npm run fix

--- src/meshes/teapot.ts
+++ src/meshes/teapot.ts
@@ -2,10 +2,10 @@ import teapotData from 'teapot';
 import { computeSurfaceNormals } from './utils';
 
 export const mesh = {
-    positions: teapotData.positions as [number,number,number][],
-    triangles: teapotData.cells as [number, number, numer][],
-    normals: [] as [number,number,number][]
+  positions: teapotData.positions as [number, number, number][],
+  triangles: teapotData.cells as [number, number, numer][],
+  normals: [] as [number, number, number][],
 };
 
 // Compute surface normals
-mesh.normals = computeSurfaceNormals(mesh.positions,mesh.triangles);
+mesh.normals = computeSurfaceNormals(mesh.positions, mesh.triangles);
