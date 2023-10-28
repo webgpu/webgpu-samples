442244e0d704ab0c479300090bdcc65f780cd76e
Gregg Tavares <github@greggman.com>
2023-10-29 08:22:20 +0900

    cleanup: remove console.log

--- src/sample/samplerParameters/main.ts
+++ src/sample/samplerParameters/main.ts
@@ -164,7 +164,6 @@ const init: SampleInit = async ({ canvas, pageState, gui }) => {
   const kScaleFactor = Math.floor(kCanvasLayoutDevicePixels / kCanvasSize);
   const kCanvasDevicePixels = kScaleFactor * kCanvasSize;
   const kCanvasCSSSize = kCanvasDevicePixels / devicePixelRatio;
-  console.log(kCanvasDevicePixels, kCanvasCSSSize);
   canvas.style.imageRendering = 'pixelated';
   canvas.width = canvas.height = kCanvasSize;
   canvas.style.minWidth = canvas.style.maxWidth = kCanvasCSSSize + 'px';
