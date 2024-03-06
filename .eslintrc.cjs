fafb9e6921de901ac7b2aaf2e0974621ed62f6d4
Gregg Tavares <github@greggman.com>
2024-03-06 00:20:53 -0800

    lint html files

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,14 +1,14 @@
 module.exports = {
-  parser: "@typescript-eslint/parser",
+  parser: '@typescript-eslint/parser',
   extends: [
-    "plugin:@typescript-eslint/recommended",
-    "plugin:prettier/recommended",
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
   ],
-  plugins: [
-    "@typescript-eslint",
-    "prettier"
-  ],
+  plugins: ['@typescript-eslint', 'eslint-plugin-html', 'prettier'],
   rules: {
-    "@typescript-eslint/no-unused-vars": ["error", { "vars": "all", "args": "after-used", "ignoreRestSiblings": true }]
+    '@typescript-eslint/no-unused-vars': [
+      'error',
+      { vars: 'all', args: 'after-used', ignoreRestSiblings: true },
+    ],
   },
 };
