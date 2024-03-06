ad1f8258daee72e259ae71ba69d34a7027502425
Gregg Tavares <github@greggman.com>
2024-03-06 13:26:48 -0800

    add redirects

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,11 @@
+import fs from 'fs';
+import path from 'path';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 import typescript from '@rollup/plugin-typescript';
 import commonjs from '@rollup/plugin-commonjs';
 import { readDirSyncRecursive } from './build/lib/readdir.js';
+
+const outPath = 'out';
 
 function wgslPlugin() {
   return {
@@ -17,6 +21,40 @@ function wgslPlugin() {
   };
 }
 
+/**
+ * Given a path like sample/foo/main.ts then, if an index.html doesn't exist
+ * in the same folder, generate a redirect index.html in the out folder.
+ * Note:
+ *    `samples/name/index.html` is a redirect (generated)
+ *    `sample/name/index.html` is the live sample (the iframe's src)
+ */
+function writeRedirect(filename) {
+  const sampleName = path.basename(path.dirname(filename));
+  const dirname = path.join(outPath, 'samples', sampleName);
+  const filepath = path.join(dirname, 'index.html');
+  fs.mkdirSync(dirname, { recursive: true });
+  console.log('created', filepath)
+  fs.writeFileSync(filepath, `\
+<!DOCTYPE html>
+<html>
+  <head>
+      <meta
+        http-equiv="refresh"
+        content="0;URL='../../?sample=${path.basename(path.dirname(filename))}'"
+      ></meta> 
+  </head>
+</html>
+`);
+}
+
+const sampleFiles = readDirSyncRecursive('sample');
+
+// Generate redirects for all samples
+sampleFiles
+  .filter((n) => n.endsWith('/index.html'))
+  .forEach(n => writeRedirect(n));
+
+
 const samplePlugins = [
   wgslPlugin(),
   nodeResolve(),
@@ -25,14 +63,14 @@ const samplePlugins = [
 ];
 
 // add a rollup rule for each sample
-const samples = readDirSyncRecursive('sample')
+const samples = sampleFiles
   .filter((n) => n.endsWith('/main.ts') || n.endsWith('/worker.ts'))
   .map((filename) => {
     return {
       input: filename,
       output: [
         {
-          file: `out/${filename.replace(/\.ts$/, '.js')}`,
+          file: `${outPath}/${filename.replace(/\.ts$/, '.js')}`,
           format: 'esm',
           sourcemap: true,
         },
@@ -46,7 +84,7 @@ export default [
     input: 'src/main.ts',
     output: [
       {
-        file: `out/main.js`,
+        file: `${outPath}/main.js`,
         format: 'esm',
         sourcemap: true,
       },
