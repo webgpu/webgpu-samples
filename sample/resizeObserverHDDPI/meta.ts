519c8b4249937d02f0c849978ca9be5e50a7aae4
Gregg Tavares <github@greggman.com>
2024-03-08 12:24:18 -0800

    fix filename

--- sample/resizeObserverHDDPI/meta.ts
+++ sample/resizeObserverHDDPI/meta.ts
@@ -1,6 +1,6 @@
 export default {
   name: 'ResizeObserver HD-DPI Fullscreen',
   description: `This example shows how to use ResizeObserver, handle HD-DPI correctly, and Fullscreen`,
-  filename: 'sample/resizeObserverHDDPI',
+  filename: __DIRNAME__,
   sources: [{ path: 'main.ts' }, { path: 'checker.wgsl' }],
 };
