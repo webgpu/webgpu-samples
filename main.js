066550e8f9e9deb3fe6a47e297255e6b261dbe76
toji <tojiro@gmail.com>
2024-03-08 22:03:41 +0000

    Deploying to gh-pages from  @ 9c58439b0d2eb71ab8ad11f2a45bc34ba001963d 🚀

--- main.js
+++ main.js
@@ -340,14 +340,16 @@ var resizeObserverHDDPI = {
 var reversedZ = {
     name: 'Reversed Z',
     description: `This example shows the use of reversed z technique for better utilization of depth buffer precision.
-    The left column uses regular method, while the right one uses reversed z technique.
-    Both are using depth32float as their depth buffer format. A set of red and green planes are positioned very close to each other.
-    Higher sets are placed further from camera (and are scaled for better visual purpose).
-    To use reversed z to render your scene, you will need depth store value to be 0.0, depth compare function to be greater,
-    and remap depth range by multiplying an additional matrix to your projection matrix.
-    Related reading:
-    https://developer.nvidia.com/content/depth-precision-visualized
-    https://web.archive.org/web/20220724174000/https://thxforthefish.com/posts/reverse_z/
+The left column uses regular method, while the right one uses reversed z technique.
+Both are using depth32float as their depth buffer format. A set of red and green planes are positioned very close to each other.
+Higher sets are placed further from camera (and are scaled for better visual purpose).
+To use reversed z to render your scene, you will need depth store value to be 0.0, depth compare function to be greater,
+and remap depth range by multiplying an additional matrix to your projection matrix.
+
+Related reading:
+  - https://developer.nvidia.com/content/depth-precision-visualized
+  - https://web.archive.org/web/20220724174000/
+  - https://thxforthefish.com/posts/reverse_z/
     `,
     filename: "sample/reversedZ",
     sources: [
@@ -523,7 +525,6 @@ const pageCategories = [
             samplerParameters,
             reversedZ,
             renderBundles,
-            spookyball,
         },
     },
     // Samples that demonstrate the GPGPU functionality of WebGPU. These samples generally provide some
