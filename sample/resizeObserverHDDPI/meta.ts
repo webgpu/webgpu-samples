b63a4a8561834876f239acea0d645ccd23e34799
Gregg Tavares <github@greggman.com>
2024-03-08 14:28:00 -0800

    add missing description

--- sample/resizeObserverHDDPI/meta.ts
+++ sample/resizeObserverHDDPI/meta.ts
@@ -1,6 +1,9 @@
 export default {
   name: 'ResizeObserver HD-DPI Fullscreen',
-  description: `This example shows how to use ResizeObserver, handle HD-DPI correctly, and Fullscreen`,
+  description: `This example shows how to use ResizeObserver, handle HD-DPI correctly, and Fullscreen
+  
+There should be no [Moiré patterns](https://www.google.com/search?q=Moir%C3%A9%20pattern) regardless of zoom level.  
+<sup>(<a href="https://caniuse.com/mdn-api_resizeobserverentry_devicepixelcontentboxsize">except possibly in Safari</a>)</sup>`,
   filename: __DIRNAME__,
   sources: [{ path: 'main.ts' }, { path: 'checker.wgsl' }],
 };
