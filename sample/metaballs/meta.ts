bc4b381f3e63ab899badda0cd6a017605797704e
Gregg Tavares <github@greggman.com>
2024-03-08 12:04:50 -0800

    fix mistakes

--- sample/metaballs/meta.ts
+++ sample/metaballs/meta.ts
@@ -1,6 +1,6 @@
 export default {
   name: 'Metaballs',
-  description: `This example shows a simple game made with WebGPU.
+  description: `This example shows an implementation of metaballs with WebGPU.
 
 Source at https://github.com/toji/webgpu-metaballs/
 `,
