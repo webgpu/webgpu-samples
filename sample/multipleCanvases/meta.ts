14472eaac20fce0e7fec46d1760e39fd6abb8873
Gregg Tavares <github@greggman.com>
2024-03-11 13:27:43 -0700

    fix description

--- sample/multipleCanvases/meta.ts
+++ sample/multipleCanvases/meta.ts
@@ -2,7 +2,7 @@ export default {
   name: 'Multiple Canvases',
   description: `\
 This example shows rendering to multiple canvases with a single WebGPU device and using \`IntersectionObserver\`
-to only render to visible canvas.
+to only render to visible canvases.
 
 For more info [see this article](https://webgpufundamentals.org/webgpu/lessons/webgpu-multiple-canvases.html).`,
   filename: __DIRNAME__,
