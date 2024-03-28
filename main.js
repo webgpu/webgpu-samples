676c203a0e6e5a30c258b72219ec0b9bcc1caf51
greggman <github@greggman.com>
2024-03-28 05:18:50 +0000

    Deploying to gh-pages from  @ 67eec2d7c15797a2b03b384d62d407e384425057 🚀

--- main.js
+++ main.js
@@ -517,6 +517,23 @@ var videoUploading = {
     ],
 };
 
+var volumeRenderingTexture3D = {
+    name: 'Volume Rendering - Texture 3D',
+    description: `This example shows how to render volumes with WebGPU using a 3D
+texture. It demonstrates simple direct volume rendering for photometric content
+through ray marching in a fragment shader, where a full-screen triangle
+determines the color from ray start and step size values as set in the vertex
+shader. This implementation employs data from the BrainWeb Simulated Brain
+Database, with decompression streams, to save disk space and network traffic.
+
+The original raw data is generated using
+[the BrainWeb Simulated Brain Database](https://brainweb.bic.mni.mcgill.ca/brainweb/)
+before processing in
+[a custom Python script](https://github.com/webgpu/webgpu-samples/tree/main/public/assets/img/volume/t1_icbm_normal_1mm_pn0_rf0.py).`,
+    filename: "sample/volumeRenderingTexture3D",
+    sources: [{ path: 'main.ts' }, { path: 'volume.wgsl' }],
+};
+
 var worker = {
     name: 'WebGPU in a Worker',
     description: `This example shows one method of using WebGPU in a web worker and presenting to
@@ -601,6 +618,7 @@ const pageCategories = [
             'a-buffer': aBuffer,
             skinnedMesh,
             textRenderingMsdf,
+            volumeRenderingTexture3D,
         },
     },
     // Samples that demonstrate how to integrate WebGPU and/or WebGPU render operations with other
