029573dbcfcb55b0a354d8156ff1fdb047e8603c
Kai Ninomiya <kainino@chromium.org>
2024-05-24 21:04:28 -0700

    Fix orientation of cubemap sample, add clearer attribution
    Fixes 341

--- sample/cubemap/meta.ts
+++ sample/cubemap/meta.ts
@@ -1,7 +1,7 @@
 export default {
   name: 'Cubemap',
   description:
-    'This example shows how to render and sample from a cubemap texture.',
+    'This example shows how to render and sample from a cubemap texture. Cubemap image available under a Creative Commons Attribution 3.0 Unported License at https://www.humus.name/index.php?page=Textures&ID=58',
   filename: __DIRNAME__,
   sources: [
     { path: 'main.ts' },
