9d9287ea0e058fe7df6b051edb6072703c25f6fb
kainino0x <kainino1@gmail.com>
2024-05-29 16:49:54 +0000

    Deploying to gh-pages from  @ bf7948f4d24bf64c0b15dd05c78aafcd09abb51b 🚀

--- main.js
+++ main.js
@@ -155,7 +155,7 @@ is used to draw instanced particles.',
 
 var cubemap = {
     name: 'Cubemap',
-    description: 'This example shows how to render and sample from a cubemap texture.',
+    description: 'This example shows how to render and sample from a cubemap texture. Cubemap image available under a Creative Commons Attribution 3.0 Unported License at https://www.humus.name/index.php?page=Textures&ID=58',
     filename: "sample/cubemap",
     sources: [
         { path: 'main.ts' },
--- sample/cubemap/main.js
+++ sample/cubemap/main.js
@@ -2604,9 +2604,13 @@ fn main(
   @location(1) fragPosition: vec4f
 ) -> @location(0) vec4f {
   // Our camera and the skybox cube are both centered at (0, 0, 0)
-  // so we can use the cube geomtry position to get viewing vector to sample the cube texture.
-  // The magnitude of the vector doesn't matter.
+  // so we can use the cube geometry position to get viewing vector to sample
+  // the cube texture. The magnitude of the vector doesn't matter.
   var cubemapVec = fragPosition.xyz - vec3(0.5);
+  // When viewed from the inside, cubemaps are left-handed (z away from viewer),
+  // but common camera matrix convention results in a right-handed world space
+  // (z toward viewer), so we have to flip it.
+  cubemapVec.z *= -1;
   return textureSample(myTexture, mySampler, cubemapVec);
 }
 `;
