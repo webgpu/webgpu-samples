035a56fc184c2d217d236a9093dc88a680e99609
Kai Ninomiya <kainino@chromium.org>
2024-09-20 14:58:09 -0700

    lint fix

--- sample/alphaToCoverage/meta.ts
+++ sample/alphaToCoverage/meta.ts
@@ -1,7 +1,6 @@
 export default {
   name: 'Alpha-to-Coverage',
-  description:
-    `
+  description: `
 Visualizes how alpha-to-coverage translates alpha values into sample
 coverage on your device. It draws two full-screen quads into a 4-sample
 texture, with the configured color and alpha values. Then, it visualizes the
@@ -13,7 +12,7 @@ device. This results in different proportions between the black background,
 the first draw, and the second draw.
 
 Examples: Some devices use 1x1 patterns, others 2x2, others 4x4. Not all devices guarantee that once a sample "pops in", it will stay there at higher alpha values. Some devices "move" samples around at certain alpha thresholds even without increasing the total sample count.
-    `,
+`,
   filename: __DIRNAME__,
   sources: [
     { path: 'main.ts' },
