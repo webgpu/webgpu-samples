aa492b60fa4a3169e77e0438468cffb1e4c20783
Thibault Coppex <tcoppex@pm.me>
2024-10-11 04:46:53 +0200

    Add marchingCubes sample (#457)

---
+++ sample/marchingCubes/meta.ts
@@ -0,0 +1,10 @@
+export default {
+  name: 'Marching Cubes',
+  description: `This example demonstrates how to dynamically generate procedural meshes using a signed distance field and a multi-pass marching cubes algorithm on the GPU.
+
+Source at <https://github.com/tcoppex/webgpu-marchingcubes>
+`,
+  filename: __DIRNAME__,
+  url: 'https://tcoppex.github.io/webgpu-marchingcubes/',
+  sources: [],
+};
