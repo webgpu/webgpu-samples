07e4a7cd9a29a0a2c4d67cf3b4a7c03f332a1203
Gregg Tavares <github@greggman.com>
2024-10-20 00:41:09 +0900

    Make build use spawnAndCheck
    I think this is the reason I didn't notice it fail.
    The build was using just spawn and so when rollup failed it
    didn't pass the exit status back out to the action so
    the action though the build succeeded.

--- build/tools/build.js
+++ build/tools/build.js
@@ -1,14 +1,14 @@
-import { spawn } from 'child_process';
 import { mkdirSync } from 'fs';
+import { spawnAndCheck } from '../lib/spawn.js';
 
 mkdirSync('out', { recursive: true });
 
-spawn('node', ['build/tools/copy.js'], {
+spawnAndCheck('node', ['build/tools/copy.js'], {
   shell: true,
   stdio: 'inherit',
 });
 
-spawn('./node_modules/.bin/rollup', ['-c'], {
+spawnAndCheck('./node_modules/.bin/rollup', ['-c'], {
   shell: true,
   stdio: 'inherit',
 });
