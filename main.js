92f8427646b8b58bff57014e91195870716b2dfa
kainino0x <kainino1@gmail.com>
2024-10-21 20:03:23 +0000

    Deploying to gh-pages from  @ 1e3c2d387d52918295b91006256fc36e48d095b4 🚀

--- main.js
+++ main.js
@@ -513,7 +513,8 @@ fonts are more complex to implement than using Canvas 2D to generate text, but t
 text looks smoother while using less memory than the Canvas 2D approach, especially at high
 zoom levels. They can be used to render larger amounts of text efficiently.
 
-The font texture is generated using [Don McCurdy's MSDF font generation tool](https://msdf-bmfont.donmccurdy.com/)`,
+The font texture is generated using [Don McCurdy's MSDF font generation tool](https://msdf-bmfont.donmccurdy.com/),
+which is built on [Viktor Chlumský's msdfgen library](https://github.com/Chlumsky/msdfgen).`,
     filename: "sample/textRenderingMsdf",
     sources: [
         { path: 'main.ts' },
