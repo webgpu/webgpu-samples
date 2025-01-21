037146cea2f072a0789709686e4b4e832bef0227
Kai Ninomiya <kainino1@gmail.com>
2025-01-21 13:24:27 -0800

    Revert "Compatibility Mode, the easy part. (#488)"
    This reverts commit 0ce1ef2b244074fd631e85c490670fc0d19d2695.

--- sample/fractalCube/main.ts
+++ sample/fractalCube/main.ts
@@ -13,9 +13,7 @@ import sampleSelfWGSL from './sampleSelf.frag.wgsl';
 import { quitIfWebGPUNotAvailable } from '../util';
 
 const canvas = document.querySelector('canvas') as HTMLCanvasElement;
-const adapter = await navigator.gpu?.requestAdapter({
-  featureLevel: 'compatibility',
-});
+const adapter = await navigator.gpu?.requestAdapter();
 const device = await adapter?.requestDevice();
 quitIfWebGPUNotAvailable(adapter, device);
 
--- sample/resizeObserverHDDPI/main.ts
+++ sample/resizeObserverHDDPI/main.ts
@@ -3,9 +3,7 @@ import checkerWGSL from './checker.wgsl';
 import { quitIfWebGPUNotAvailable } from '../util';
 
 const canvas = document.querySelector('canvas') as HTMLCanvasElement;
-const adapter = await navigator.gpu?.requestAdapter({
-  featureLevel: 'compatibility',
-});
+const adapter = await navigator.gpu?.requestAdapter();
 const device = await adapter?.requestDevice();
 quitIfWebGPUNotAvailable(adapter, device);
 
