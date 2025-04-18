1b10a3983df470784d0c739db0d9851bf03d316b
beaufortfrancois <beaufort.francois@gmail.com>
2025-04-18 08:58:54 +0000

    Deploying to gh-pages from  @ a1bafc99b8acdeb928158e0efd2f695d25550b85 🚀

--- sample/occlusionQuery/main.js
+++ sample/occlusionQuery/main.js
@@ -8273,6 +8273,16 @@ struct VSOut {
 }
 `;
 
+// Show an error dialog if there's any uncaught exception or promise rejection.
+// This gets set up on all pages that include util.ts.
+window.addEventListener('unhandledrejection', (ev) => {
+    fail(`unhandled promise rejection, please report a bug!
+  https://github.com/webgpu/webgpu-samples/issues/new\n${ev.reason}`);
+});
+window.addEventListener('error', (ev) => {
+    fail(`uncaught exception, please report a bug!
+  https://github.com/webgpu/webgpu-samples/issues/new\n${ev.error}`);
+});
 /** Shows an error dialog if getting an adapter wasn't successful. */
 function quitIfAdapterNotAvailable(adapter) {
     if (!('gpu' in navigator)) {
