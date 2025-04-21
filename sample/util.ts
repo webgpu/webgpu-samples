0d9da4af11b65e94927c1099485e4dc501149f63
Kai Ninomiya <kainino@chromium.org>
2025-04-21 16:18:57 -0700

    Forward errors from workers to show a dialog

--- sample/util.ts
+++ sample/util.ts
@@ -3,11 +3,15 @@ export function quitIfAdapterNotAvailable(
   adapter: GPUAdapter | null
 ): asserts adapter {
   if (!('gpu' in navigator)) {
-    fail('navigator.gpu is not defined - WebGPU not available in this browser');
+    throw fail(
+      'navigator.gpu is not defined - WebGPU not available in this browser'
+    );
   }
 
   if (!adapter) {
-    fail("requestAdapter returned null - this sample can't run on this system");
+    throw fail(
+      "requestAdapter returned null - this sample can't run on this system"
+    );
   }
 }
 
@@ -21,7 +25,7 @@ export function quitIfLimitLessThan(
     const limitKey = limit as keyof GPUSupportedLimits;
     const limitValue = adapter.limits[limitKey] as number;
     if (limitValue < requiredValue) {
-      fail(
+      throw fail(
         `This sample can't run on this system. ${limit} is ${limitValue}, and this sample requires at least ${requiredValue}.`
       );
     }
@@ -39,8 +43,7 @@ export function quitIfWebGPUNotAvailable(
 ): asserts device {
   if (!device) {
     quitIfAdapterNotAvailable(adapter);
-    fail('Unable to get a device for an unknown reason');
-    return;
+    throw fail('Unable to get a device for an unknown reason');
   }
 
   device.lost.then((reason) => {
@@ -51,16 +54,46 @@ export function quitIfWebGPUNotAvailable(
   });
 }
 
-/** Fail by showing a console error, and dialog box if possible. */
-const fail = (() => {
+/**
+ * Create a MessageChannel, and forward messages to fail() to show an error
+ * dialog. Return a MessagePort for the worker to send messages back on.
+ */
+export function mainThreadCreateErrorMessagePortForWorker() {
+  if (typeof window === 'undefined') throw new Error('Called on wrong thread!');
+  const mc = new MessageChannel();
+  mc.port1.onmessage = (ev: MessageEvent<string>) => {
+    fail(ev.data);
+  };
+  return mc.port2;
+}
+
+let errorMessagePort: MessagePort | null = null;
+export function workerRegisterErrorMessagePort(port: MessagePort) {
+  if (typeof window !== 'undefined') throw new Error('Called on wrong thread!');
+  errorMessagePort = port;
+}
+
+/**
+ * Fail by showing a console error, and dialog box if possible.
+ *
+ * Returns an Error object, which may be thrown if execution should stop here.
+ * (Throwing the error will generally trigger one of the global listeners,
+ * 'unhandledrejection' or 'error', but this won't do anything because the
+ * dialog is already open at that point, and we don't overwrite it.)
+ */
+const fail: (message: string) => Error = (() => {
   type ErrorOutput = { show(msg: string): void };
 
   function createErrorOutput() {
     if (typeof document === 'undefined') {
-      // Not implemented in workers.
       return {
         show(msg: string) {
-          console.error(msg);
+          if (errorMessagePort) {
+            errorMessagePort.postMessage(msg);
+          } else {
+            console.warn('workerRegisterErrorMessagePort has not been called!');
+            console.error(msg);
+          }
         },
       };
     }
@@ -96,6 +129,6 @@ const fail = (() => {
     if (!output) output = createErrorOutput();
 
     output.show(message);
-    throw new Error(message);
+    return new Error(message);
   };
 })();
