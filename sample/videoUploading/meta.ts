22cebbf4ce2d6e52212a9907ff5f1b3238bca5dc
Greggman <github@greggman.com>
2026-01-05 11:24:30 -0800

    Remove the LHC video since we can't find attribution (#551)
    Gemini claims it's from the "CERN 360" project (often called the "CERN
    Street View"), which was a collaboration between Google Arts & Culture
    and CERN but I couldn't find any trace of it. I did find this:
    https://europe.googleblog.com/2013/09/street-view-arrives-at-cern.html
    but it's street view stills only.
    
    I like this video because it's dynamic, going down the LHC tunnel at a
    quick pace, and because being in a tunnel with stuff close by they go
    past the camera quicky. CERN has other 360 videos but none of them are
    as dynamic. There's this one https://www.youtube.com/watch?v=NBrVCBCEVfk
    but it's only got a small slow portion in the tunnel.
    
    Their license is here: https://copyright.web.cern.ch/
    
    It's a little surprising how few 360 videos there are. There's even less
    180 stereoscopic videos except for adult content.
    
    * #550

--- sample/videoUploading/meta.ts
+++ sample/videoUploading/meta.ts
@@ -3,7 +3,6 @@ export default {
   description: `\
 This example shows how to upload video frame to WebGPU.
 giraffe by [Taryn Elliott](https://www.pexels.com/video/giraffe-walking-in-the-forest-5214261/).
-lhc by [unknown](https://foo.com).
 lake by [Fabio Casati](https://commons.wikimedia.org/wiki/File:Video_360%C2%B0._Timelapse._Bled_Lake_in_Slovenia..webm), [CC BY 3.0](https://creativecommons.org/licenses/by/3.0)
 `,
   filename: __DIRNAME__,
